332245c2e3eb66ec604f7959083360ae614e1479
Kang <zhengkang86@gmail.com>
2020-09-03 21:42:20 -0400

    [Bug fix]: solve empty ROI problem, improve support image search efficiency

--- fss/coco_few_shot.py
+++ fss/coco_few_shot.py
@@ -10,7 +10,7 @@ import skimage.io as io
 
 
 class FewShotInstData(data.Dataset):
-    def __init__(self, coco_dir, subset, folds, n_shots, img_size=224, exclude_list_file=None):
+    def __init__(self, coco_dir, subset, folds, n_shots, img_size=224, exclude_list_file=None, resize_flag=True):
         '''
         :param coco_dir: The path of COCO folder.
         :param subset: 'train2017', 'val2017' or 'test2017'.
@@ -25,18 +25,27 @@ class FewShotInstData(data.Dataset):
         for fold in folds:
             active_cat_ids += all_cat_ids[fold*20:(fold+1)*20]
 
-        self.inst_ids = list()
-        for cat_id in active_cat_ids:
-            self.inst_ids += self.coco.getAnnIds(catIds=cat_id, iscrowd=False)
-
         if 'train' in subset and exclude_list_file is not None:
             excluded_inst_ids = json.load(open(exclude_list_file))
-            self.inst_ids = list(set(self.inst_ids) - set(excluded_inst_ids))
+        else:
+            excluded_inst_ids = []
+
+        self.inst_ids = list()
+        self.cat_inst_ids = {}
+        for cat_id in active_cat_ids:
+            self.cat_inst_ids[cat_id] = self.coco.getAnnIds(catIds=cat_id, iscrowd=False)
+            if excluded_inst_ids:
+                self.cat_inst_ids[cat_id] = list(set(self.cat_inst_ids[cat_id]) - set(excluded_inst_ids))
+            self.inst_ids += self.cat_inst_ids[cat_id]
+        print('Total inst. #:', len(self.inst_ids))
+        # cat_insts_cnt = {cat_id: len(cat_inst_ids) for cat_id, cat_inst_ids in self.cat_inst_ids.items()}
+        # print(cat_insts_cnt)
 
         self.n_shots = n_shots
         self.img_size = img_size
         self.coco_dir = coco_dir
         self.subset = subset
+        self.resize_flag = resize_flag
 
     def __len__(self):
         return len(self.inst_ids)
@@ -69,9 +78,16 @@ class FewShotInstData(data.Dataset):
         y_min = max(0, np.round(y_min).astype(np.int))
         y_max = min(img.shape[0] - 1, np.round(y_max).astype(np.int))
         roi = img[y_min:y_max, x_min:x_max].copy()
-        # roi = cv2.resize(roi, (self.img_size, self.img_size))
         roi_mask = img_mask[y_min:y_max, x_min:x_max].copy()
-        # roi_mask = cv2.resize(roi_mask, (self.img_size, self.img_size))
+
+        if self.resize_flag:
+            try:
+                roi = cv2.resize(roi, (self.img_size, self.img_size))
+                roi_mask = cv2.resize(roi_mask, (self.img_size, self.img_size))
+            except:
+                print(img_id, inst_id, img_dict['file_name'], inst_cat_id)
+                print('Orig:', x_min, y_min, x_max, y_max, w, h)
+                raise ValueError('ROI error')
 
         inst = {
             "inst_id": inst_id,
@@ -86,7 +102,7 @@ class FewShotInstData(data.Dataset):
         query_inst_id = self.inst_ids[idx]
         query_inst = self.getInstByID(query_inst_id)
 
-        same_cat_inst_ids = self.coco.getAnnIds(catIds=query_inst["inst_cat_id"], iscrowd=False)
+        same_cat_inst_ids = self.cat_inst_ids[query_inst["inst_cat_id"]].copy()
         same_cat_inst_ids.remove(query_inst_id)
         support_inst_ids = random.choices(same_cat_inst_ids, k=self.n_shots)
         support_insts = [self.getInstByID(inst_id) for inst_id in support_inst_ids]
